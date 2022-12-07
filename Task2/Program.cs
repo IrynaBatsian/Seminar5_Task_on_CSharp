a6ceac33ad9d87284f86232afc9c396689f38d7e
Iryna <batsianiryna@gmail.com>
2022-12-08 00:23:01 +0300

    add task2 замена элементов массива положительный на отрицательные и наоборот

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+void InputArray(int[] array) // Процедура для заполнения массива
+{
+  for (int i = 0; i < array.Length; i++)
+      array[i] = new Random().Next();   
+}
+
+
+void ReleaseArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++) // Прохождение по массиву
+  array[i] *= (-1);
+}  
+
+
+
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n]; // Создаем сам массив в котором количество элементов = n.
+InputArray(array); // Процедура заполнения нашего одномерного массива
+Console.WriteLine($"Исходный массив: [{string.Join(", ", array)}]"); // Выводим заданный массив
+ReleaseArray(array); // Запускаем процедуру
+Console.WriteLine($"Конечный массив: [{string.Join(", ", array)}]");
